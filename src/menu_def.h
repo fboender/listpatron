152d129f95bf71f868a9276b9828500b7a15448c
todsah <todsah@581e4e78-6143-4e7e-ada4-f34e045b94a3>
2004-12-22 18:55:27 +0000

    Initial commit.
    git-svn-id: https://svn.electricmonk.nl/svn/listpatron/trunk@72 581e4e78-6143-4e7e-ada4-f34e045b94a3

---
+++ src/menu_def.h
@@ -0,0 +1,105 @@
+/****************************************************************************
+ * New Menu
+ ****************************************************************************/
+
+/* Normal items */
+static GtkActionEntry entries[] = {
+	{ "FileMenu"      , NULL             , "_File" }              , 
+	{ "New"           , GTK_STOCK_NEW    , "_New"                 , "<control>N" , "Create a new file"                           , ui_menu_file_new_cb }            , 
+	{ "Open"          , GTK_STOCK_OPEN   , "_Open"                , "<control>O" , "Open a file"                                 , ui_menu_file_open_cb }           , 
+	{ "Save"          , GTK_STOCK_SAVE   , "_Save"                , "<control>S" , "Save the current file"                       , ui_menu_file_save_cb }           , 
+	{ "SaveAs"        , NULL             , "Save _As"             , NULL         , "Save the file under a different name"        , ui_menu_file_save_as_cb }        , 
+	{ "ImportMenu"    , NULL             , "_Import" }            , 
+	{ "ImportCSV"     , GTK_STOCK_OPEN   , "_Character Seperated" , NULL         , "Import from character seperated values file" , ui_menu_file_import_csv_cb }     , 
+	{ "ExportMenu"    , NULL             , "_Export" }            , 
+	{ "ExportCSV"     , GTK_STOCK_SAVE   , "_Character Seperated" , NULL         , "Export to character seperated values file"   , ui_menu_file_export_csv_cb }     , 
+	{ "ExportPS"      , GTK_STOCK_SAVE   , "_PostScript"          , NULL         , "Export to PostScript file"                   , ui_menu_file_export_ps_cb }      , 
+	{ "ExportHTML"    , GTK_STOCK_SAVE   , "_HTML"                , NULL         , "Export to a HTML file"                       , ui_menu_file_export_html_cb }    , 
+	{ "Exit"          , GTK_STOCK_QUIT   , "E_xit"                , NULL         , "Exit the program"                            , ui_menu_file_quit_cb }           , 
+
+	{ "EditMenu"      , NULL             , "_Edit" }              , 
+	{ "Cut"           , GTK_STOCK_CUT    , "_Cut"                 , "<control>X" , "Cut selection" }                             , 
+	{ "Copy"          , GTK_STOCK_COPY   , "_Copy"                , "<control>C" , "Copy selection to the clipboard" }           , 
+	{ "Paste"         , GTK_STOCK_PASTE  , "_Paste"               , "<control>V" , "Paste selection from the clipboard"  }       , 
+	{ "Find"          , GTK_STOCK_FIND   , "_Find"                , "<control>F" , "Find a value"                                , ui_menu_edit_find_cb }           , 
+
+	{ "DataMenu"      , NULL             , "_Data" }              , 
+	{ "ColumnMenu"    , NULL             , "_Columns" }           , 
+	{ "ColAdd"        , GTK_STOCK_ADD    , "_Add"                 , "<alt>a"     , "Add a column to the list"                    , ui_menu_column_add_cb }          , 
+	{ "ColDel"        , GTK_STOCK_DELETE , "_Delete"              , "<alt>d"     , "Delete the current column"                   , ui_menu_column_delete_cb }       , 
+	{ "ColRename"     , NULL             , "_Rename"              , "<alt>r"     , "Rename the current column"                   , ui_menu_column_rename_cb }       , 
+	{ "RowMenu"       , NULL             , "_Rows" }              , 
+	{ "RowAdd"        , GTK_STOCK_ADD    , "_Add"                 , NULL         , "Add a row to the list"                       , ui_menu_row_add_cb }             , 
+	{ "RowDel"        , GTK_STOCK_DELETE , "_Delete"              , NULL         , "Delete the current row"                      , ui_menu_row_delete_cb }          , 
+	{ "SortMenu"      , NULL             , "_Sort" }              , 
+	{ "SortEdit"      , NULL             , "_Edit sorting rules"  , NULL         , "Add                                          , change and delete sorting rules" , ui_menu_sort_rules_cb } , 
+	{ "SortNew"       , NULL             , "_New sorting rule"    , NULL         , "Add a sorting rule"                          , ui_menu_sort_edit_cb }           , 
+	{ "FilterMenu"    , NULL             , "_Filter" }            , 
+	{ "ReportMenu"    , NULL             , "_Report" }            , 
+
+#ifdef _DEBUG
+	{ "DebugMenu"     , NULL             , "De_bug" }             , 
+	{ "AddTestData"   , NULL             , "Add test _data"       , NULL         , "Add test data to an empty list"              , ui_menu_debug_addtestdata_cb }   , 
+	{ "AddTestRows"   , NULL             , "Add test _rows"       , NULL         , "Add test rows to a testdata list"            , ui_menu_debug_addtestrows_cb }   , 
+	{ "DumpSortRules" , NULL             , "Dump _sorting rules"  , NULL         , "Dump the available sorting rules"            , list_sort_dump_rules }           , 
+#endif
+};
+
+static const char *ui_description =
+"<ui>"
+"  <menubar name=\"MainMenu\">"
+"    <menu action=\"FileMenu\">"
+"      <menuitem action=\"New\"/>"
+"      <menuitem action=\"Open\"/>"
+"      <menuitem action=\"Save\"/>"
+"      <menuitem action=\"SaveAs\"/>"
+"      <menu action=\"ImportMenu\">"
+"        <menuitem action=\"ImportCSV\" />"
+"      </menu>"
+"      <menu action=\"ExportMenu\">"
+"        <menuitem action=\"ExportCSV\" />"
+"        <menuitem action=\"ExportPS\" />"
+"        <menuitem action=\"ExportHTML\" />"
+"      </menu>"
+"      <menuitem action=\"Exit\"/>"
+"    </menu>"
+"    <menu action=\"EditMenu\">"
+"      <menuitem action=\"Cut\"/>"
+"      <menuitem action=\"Copy\"/>"
+"      <menuitem action=\"Paste\"/>"
+"      <separator/>"
+"      <menuitem action=\"Find\"/>"
+"    </menu>"
+"    <menu action=\"DataMenu\">"
+"      <menu action=\"ColumnMenu\">"
+"        <menuitem action=\"ColAdd\"/>"
+"        <menuitem action=\"ColDel\"/>"
+"        <menuitem action=\"ColRename\"/>"
+"      </menu>"
+"      <menu action=\"RowMenu\">"
+"        <menuitem action=\"RowAdd\"/>"
+"        <menuitem action=\"RowDel\"/>"
+"      </menu>"
+"      <separator/>"
+"      <menu action=\"SortMenu\">"
+"        <menuitem action=\"SortEdit\"/>"
+"        <menuitem action=\"SortNew\"/>"
+"        <separator/>"
+"      </menu>"
+"      <menu action=\"FilterMenu\">"
+"      </menu>"
+"      <menu action=\"ReportMenu\">"
+"      </menu>"
+"    </menu>"
+#ifdef _DEBUG
+"    <menu action=\"DebugMenu\">"
+"      <menuitem action=\"AddTestData\"/>"
+"      <menuitem action=\"AddTestRows\"/>"
+"      <separator/>"
+"      <menuitem action=\"DumpSortRules\"/>"
+"    </menu>"
+#endif
+"  </menubar>"
+"</ui>";
+
+
