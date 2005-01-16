ee9ee2de495f5f580f3b89cec271d93af3b5b97f
todsah <todsah@581e4e78-6143-4e7e-ada4-f34e045b94a3>
2005-01-16 18:16:11 +0000

    Now initializes complete development environment.
    git-svn-id: https://svn.electricmonk.nl/svn/listpatron/trunk@114 581e4e78-6143-4e7e-ada4-f34e045b94a3

--- reconf.sh
+++ reconf.sh
@@ -2,4 +2,19 @@
 #
 # Custom script
 #
+# This script prepares a cvs checkout of ListPatron for development.
+# It reruns various autotools, creates some symlinks and configures the
+# sources so you can start developing in the src/ dir.
+#
+# Also run this tool whenever you make changes to any of the .am or .in 
+# files.
+#
+echo "Running autotools.."
 aclocal && autoheader && automake --add-missing --copy && autoconf
+echo "Creating synthetic /usr/share in current dir.."
+rm pixmaps/listpatron
+ln -s . pixmaps/listpatron
+rm xml/listpatron
+ln -s . xml/listpatron
+echo "Configuring the source (debugging enabled).."
+./configure --datadir=`pwd` --enable-debug
